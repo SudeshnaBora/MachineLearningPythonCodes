055db77b11c28f08792f9c3e0baf719de188f55a
SudeshnaBora <sudeshnamani1@gmail.com>
2015-09-22 11:40:12 +0530

    New update

--- KnnAlgorithm/Knn.py
+++ KnnAlgorithm/Knn.py
@@ -30,29 +30,60 @@ def classifyKnn(inX,dataSet,labels,k):
     return sortedClassCount[0][0]
 
 def ReadData():
-    filePath = "D:\GithubRepositories\MachineLearningPythonCodes\KnnAlgorithm\datingTestSet.txt"
+     filePath = "D:\GithubRepositories\MachineLearningPythonCodes\KnnAlgorithm\datingTestSet.txt"
 
-    with open(filePath) as file:
-        numberOfLines = len(file.readlines())
-        propertyMatrix = np.zeros((numberOfLines,3))
-        Label = []
+     with open(filePath) as file:
+         SmallIndexTotal = 0
+         NoIndexTotal = 0
+         LargeIndexTotal =0
+         for line in file.readlines():
+             line = line.strip()
+             listFromLine = line.split('\t')
+             if listFromLine[-1]== 'smallDoses':
+                 SmallIndexTotal +=1
+             elif listFromLine[-1]== 'largeDoses':
+                 LargeIndexTotal += 1
+             else:
+                 NoIndexTotal +=1
+         SmallMat = np.zeros((SmallIndexTotal,3))
+         NoMat = np.zeros((NoIndexTotal,3))
+         LargeMat = np.zeros((LargeIndexTotal,3))
+         Labels = []
+     with open(filePath) as fid:
+         numberOfLines = len(fid.readlines())
+         propertyMatrix = np.zeros((numberOfLines,3))
 
-    with open(filePath) as fr:
-        index=0
-        for line in fr.readlines():
-            line = line.strip()
-            listFromLine = line.split('\t')
-            propertyMatrix[index,:] = listFromLine[0:3]
-            Label.append(listFromLine[-1])
-            index +=1
-    return propertyMatrix,Label
+     with open(filePath) as fr:
+         SmallIndex=0
+         NoIndex=0
+         LargeIndex=0
+         Index = 0
+         for line in fr.readlines():
+             line = line.strip()
+             listFromLine = line.split('\t')
+             propertyMatrix[Index,:] = listFromLine[0:3]
+             Index += 1
+             if listFromLine[-1]== 'smallDoses':
+                 SmallMat[SmallIndex,:]= listFromLine[0:3]
+                 SmallIndex +=1
+             elif listFromLine[-1]== 'largeDoses':
+                 LargeMat[LargeIndex,:]= listFromLine[0:3]
+                 LargeIndex +=1
+             else:
+                 NoMat[NoIndex,:]= listFromLine[0:3]
+                 NoIndex +=1
+             Labels.append(listFromLine[-1])
+     return SmallMat,NoMat,LargeMat,propertyMatrix,Labels
     
-def plotting(propMat,Labels):
+def plotting(NoMat,SmallMat,LargeMat):
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    ax.scatter(propMat[:,1], propMat[:,2])
+    a1=ax.scatter(NoMat[:,0], NoMat[:,1],c='b',label='Didn\'t like')
+    a2=ax.scatter(SmallMat[:,0], SmallMat[:,1],c='g',label='Small doses')
+    a3=ax.scatter(LargeMat[:,0], LargeMat[:,1],c='r',label='Large doses')
     plt.title("Characteristics")
-    plt.xlabel("Ice Cream Consumption")
+    plt.xlabel("Flying Miles")
     plt.ylabel("Video Game Duration")
+    plt.legend(handles=[a1,a2,a3],loc = 'upper left')
     plt.show()
 
